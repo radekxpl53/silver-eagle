072716e9ee063158fc0f1b5abfde6bf707611bb5
Deffryda <80837333+Deffryda@users.noreply.github.com>
2026-01-24 14:37:08 +0100

    Prototyp

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -1,22 +1,46 @@
 using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Asteroid : MonoBehaviour
 {
     public List<AsteroidMaterialEntry> materials;
+    public TextMeshProUGUI info;
+
+    private List<float> GetWeightedTemps()
+    {
+        float sum = materials.Sum(m => m.amount);
+        return materials
+            .Select(m => m.material.baseTemperature * (1 + m.amount / sum))
+            .ToList();
+    }
+
 
     public float ToleranceTemperature()
     {
-        float sum = materials.Sum(m => m.amount);
-        List<float> temps = new();
-
-        foreach (var m in materials)
-        {
-            float t = m.material.baseTemperature * (1 + m.amount / sum);
-            temps.Add(t);
-        }
-
+        var temps = GetWeightedTemps();
         return Mathf.Ceil(temps.Min() * 0.8f + temps.Average() * 0.2f);
+    }
+
+    public float CalculateTemperature()
+    {
+        var temps = GetWeightedTemps();
+        return Mathf.Ceil(temps.Max() * 0.8f + temps.Average() * 0.2f);
+    }
+
+
+    public void showInfoAsteroid(){
+        string textInfo = $"Temperatura: {CalculateTemperature()} \u00B0C" +
+        $"\nTolerancja: {ToleranceTemperature()} \u00B0C" +
+        $"\nSurowce:\n";
+        foreach(var m in materials){
+            textInfo += $"{m.material.materialName} - {m.amount}\n";
+        }
+        info.text = textInfo;
+    }
+
+    void Start(){
+        showInfoAsteroid();
     }
 }
