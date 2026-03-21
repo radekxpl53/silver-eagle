e30a7769c444316d593695c574c80b910fd6e8ea
Kaparee <jakubplocicati@gmail.com>
2026-03-22 00:33:11 +0100

    fix 2

--- Assets/Scripts/Economy & Inventory/SellSystem.cs
+++ Assets/Scripts/Economy & Inventory/SellSystem.cs
@@ -48,7 +48,7 @@ public class SellSystem : MonoBehaviour
         if (endScreen != null)
         {
             endScreen.SetActive(true);
-            Cursor.lockState = CursorLockMode.None;
+            Cursor.lockState = CursorLockMode.Confined;
             Cursor.visible = true;
         }
     }
