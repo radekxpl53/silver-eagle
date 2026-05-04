ad9cbf49dac09d6fb348bf3de44cafaef66a9827
hype258 <141255923+hype258@users.noreply.github.com>
2026-05-04 21:52:00 +0200

    chyba git

--- Assets/Scripts/Player/ShipStats.cs
+++ Assets/Scripts/Player/ShipStats.cs
@@ -31,6 +31,9 @@ public class ShipStats : MonoBehaviour {
     [SerializeField] private float normalDrainRate = 1f;
     [SerializeField] private float lowFuelThreshold = 40f;
 
+    public float baseCargoCapacity = 100f;
+    public float currentMaxCargo;
+
     [Header("--- SKRYPTY STERUJĄCE DO ZABLOKOWANIA ---")]
     [SerializeField] private MonoBehaviour[] controlScriptsToDisable;
 
@@ -55,6 +58,16 @@ public class ShipStats : MonoBehaviour {
         CurrentCargo = 0;
     }
 
+    //*****************************88888  
+    public void UpdateMaxCargo(float multiplier) 
+    {
+        MaxCargo = baseCargoCapacity * multiplier;
+        Debug.Log($"[ShipStats] Nowy limit ładowni: {MaxCargo}");
+    }
+
+    
+
+    //****************88888
     public void TakeDamage(float damage)
     {
         if (damage > 0f)
